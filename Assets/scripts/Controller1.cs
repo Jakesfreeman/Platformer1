5beae72e5105afdc2ff4d8e5793db272e90cbeb5
jake <jakesfreeman98@gmail.com>
2020-03-25 13:52:32 -0700

    2nd milestone start work

--- Assets/scripts/Controller1.cs
+++ Assets/scripts/Controller1.cs
@@ -8,6 +8,7 @@ public class Controller1 : MonoBehaviour
     private Transform tf; // A variable to hold our Transform component
     private SpriteRenderer sr;
     private Rigidbody2D rb2d;
+    private Animator anim;
 
     public float jumpForce = 3.5f;
     public float speed = 5.0f; // a speed variable that designers can change in unity
@@ -18,6 +19,7 @@ public class Controller1 : MonoBehaviour
         tf = GetComponent<Transform>(); // gets the transform to use for movement
         sr = GetComponent<SpriteRenderer>();
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -28,7 +30,20 @@ public class Controller1 : MonoBehaviour
         rb2d.velocity = new Vector2(xMovemoent,rb2d.velocity.y);
 
         sr.flipX = rb2d.velocity.x < 0;
-
+        if (xMovemoent > 0)
+        {
+            sr.flipX = false;
+            anim.Play("Run_player");
+        }
+        else if(xMovemoent < 0)
+        {
+            sr.flipX = true;
+            anim.Play("Run_player");
+        }
+        else
+        {
+            anim.Play("Player_idle");
+        }
         //checking for isGrounded bool
         
         RaycastHit2D hitInfo = Physics2D.Raycast(groundPoint.position, Vector2.down, 0.1f);
@@ -40,10 +55,30 @@ public class Controller1 : MonoBehaviour
         {
             isGrounded = false;
         }
-        //jump
+        //working with animation transitons
+        if (xMovemoent == 0.0f)
+        {
+            anim.SetBool("isRunning", false);
+        }
+        else
+        {
+            anim.SetBool("isRunning", true);
+        }
+
+        //jump and trying to only play animation when space is pressed
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb2d.AddForce((Vector2.up * jumpForce));
+            anim.SetBool("isGrounded", false);
+        }
+        else
+        {
+            anim.SetBool("isGrounded", true);
+        }
+
+        if (anim.GetBool("isGrounded") == false)
+        {
+            anim.Play("Player_jump");
         }
     }
 }
